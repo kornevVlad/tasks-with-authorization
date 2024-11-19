1b0df724dd7fc4fa7e4548d78523b2c18ffd3e53
kornevVlad <vlad1990@yandex.ru>
2024-11-19 12:56:23 +0300

    создание проекта

---
+++ tasks-with-authorization/src/main/java/com/my/project/tasks_with_authorization/TasksWithAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.my.project.tasks_with_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksWithAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksWithAuthorizationApplication.class, args);
+	}
+
+}
---
+++ tasks-with-authorization/src/test/java/com/my/project/tasks_with_authorization/TasksWithAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.project.tasks_with_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksWithAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
